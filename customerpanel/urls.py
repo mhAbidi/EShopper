fd4a7b51fb47bfc217c9ccc101b6b8817bd3079a
mhAbidi <mohdhusain.1525@gmail.com>
2021-04-27 17:45:36 +0530

    commit

--- customerpanel/urls.py
+++ customerpanel/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from customerpanel import views
 
 urlpatterns = [
-    path('',views.nf404,name='404')
+    path('',views.index,name=''),
+    path('404',views.nf404,name='404'),
+    path('blog',views.blog,name='blog'),
+    path('index',views.index,name='index'),
+    path('shop',views.shop,name='shop'),
+    path('product-details',views.product_details,name='product-details'),
+    path('login',views.login,name='login'),
+    path('contact-us',views.contact,name='contact-us'),
+    path('checkout',views.checkout,name='checkout'),
+    path('cart',views.cart,name='cart')
+
 ]
--- customerpanel/views.py
+++ customerpanel/views.py
@@ -10,10 +10,33 @@ def nf404(request):
     return render(request,'customerpanel/404.html')
 
 def index(request):
-    return HttpResponse("Hello")
-    #django.template.loaders.app_directories.Loader: C:\Users\user\Desktop\Neosoft\EShopper\eshopper\adminpanel\templates\customerpanel\404.html (Source does not exist)
-    #return render(request,'customerpanel/404.html') 
-
-def nff404(request):
-    return render(request,'customerpanel/404.html')
     #return HttpResponse("Hello")
+    #django.template.loaders.app_directories.Loader: C:\Users\user\Desktop\Neosoft\EShopper\eshopper\adminpanel\templates\customerpanel\404.html (Source does not exist)
+    return render(request,'customerpanel/index.html')
+
+def blog(request):
+    return HttpResponse("Blog placeholder")
+
+def cart(request):
+    return render(request, 'customerpanel/cart.html')
+
+
+def product_details(request):
+    return render(request,'customerpanel/product-details.html')
+
+def login(request):
+    return render(request,'customerpanel/login.html')
+
+def shop(request):
+    return render(request,'customerpanel/shop.html')
+
+def contact(request):
+    return render(request, 'customerpanel/contact-us.html')
+
+def checkout(request):
+    return render(request, 'customerpanel/checkout.html')
+
+
+
+
+
