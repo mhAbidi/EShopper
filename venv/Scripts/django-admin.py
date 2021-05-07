fe39325dcabcda893eb65846a0fd682737bc27b2
mhAbidi <58091172+mhAbidi@users.noreply.github.com>
2021-05-07 09:47:22 +0530

    Delete venv directory

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\user\Desktop\Neosoft\EShopper\eshopper\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
